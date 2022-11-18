a4612280693f4a623e935ba7b7b85f01eb25fef9
AlienNT <nick.tristen22@gmail.com>
2022-11-18 19:29:33 +0200

    new добавил компоненты для отображения навыков

---
+++ src/components/content/about/VSkillsList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="v-skills v-col v-transition">
+    <ul class="v-skills__list v-row v-transition">
+      <VSkillCard
+          v-for="skill in skills"
+          :key="skill?.id"
+          :data="skill"
+          :columns="columns"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import VSkillCard from "@/components/content/about/VSkillCard";
+
+export default {
+  name: "VSkillsList",
+  props: {
+    skills: {
+      type: Array,
+      default: null
+    },
+    columns: {
+      type: Number,
+      default: 4
+    }
+  },
+  components: {
+    VSkillCard
+  }
+}
+</script>
+
+<style scoped>
+.v-skills__list {
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+</style>
