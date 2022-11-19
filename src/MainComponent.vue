3fd2c4da43d416d4b789e8754022a41c5f510a86
AlienNT <nick.tristen22@gmail.com>
2022-11-19 06:12:37 +0200

    fix правки стилей и компонентов

--- src/MainComponent.vue
+++ src/MainComponent.vue
@@ -1,18 +1,25 @@
 <template>
   <div
       class="v-main-component"
+      :class="{
+        'mobile-app': isMobile
+      }"
   >
     <div class="v-sidebar-component">
       <VSidebar
           :navLinks="navLinks"
           :isMobile="isMobile"
+          @onClick="setScrollEvent"
       />
     </div>
-    <div class="v-content-component">
-      <div class="v-container">
-        <VContent/>
+    <div class="v-content-component"
+         ref="v-content">
+
+        <VContent
+            :isMobile="isMobile"
+        />
       </div>
-    </div>
+
   </div>
 </template>
 
@@ -20,6 +27,7 @@
 import VSidebar from "@/components/sidebar/VSidebar";
 import VContent from "@/components/content/VContent";
 import {_notEmpty} from "@/helpers/fakeLodash";
+import {setTitle} from "@/helpers/apiHelper";
 
 export default {
   name: "MainComponent",
@@ -40,13 +48,28 @@ export default {
       //todo адаптив для малых экранов
       return this.windowWidth < 500
     },
+    routeName() {
+      return this.$route['name']
+    }
+  },
+  watch: {
+    routeName: {
+      handler(e) {
+        setTitle(e)
+
+      },
+      immediate: true
+    }
   },
   methods: {
+    setScrollEvent (event) {
+      this.$store.dispatch('scrollEvent', event)
+    },
     setInnerWidth() {
       this.windowWidth = window.innerWidth
     },
     async getNavLinks() {
-     return await this.$store.dispatch('fetchNavLinks')
+      return await this.$store.dispatch('fetchNavLinks')
       /*return await axios(
           {
             url: routes.NAV_LINKS,
@@ -73,31 +96,60 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
 @import "assets/css/main.scss";
 
 .v-main-component {
   background: #ffffff;
   display: flex;
   width: 100%;
+  height: 100vh;
 }
 
 .v-sidebar-component, .v-content-component {
   height: 100vh;
-  overflow-y: auto;
   display: flex;
 }
 
 .v-sidebar-component {
   min-width: fit-content;
-  overflow-x: hidden;
-  overflow-y: auto;
 }
 
 .v-content-component {
-  padding-top: 15px;
-  padding-bottom: 15px;
+  background-image: url("assets/img/background.webp");
+  background-repeat: no-repeat;
+
+  overflow-y: auto;
   flex: 1 1 auto;
+  overflow-x: hidden;
+  .v-content {
+    background: rgba(50, 44, 61, 0.8);
+  }
 }
 
+
+.mobile-app {
+  flex-direction: column;
+
+  .v-sidebar-component {
+    height: fit-content;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .v-content-component {
+    padding: 0;
+
+    > .v-container {
+      max-width: 100%;
+      padding: 0;
+
+      .v-content {
+        border-radius: 0;
+      }
+    }
+  }
+
+}
 </style>
--- src/components/content/VContent.vue
+++ src/components/content/VContent.vue
@@ -1,47 +1,92 @@
 <template>
   <main class="v-content">
-    <div class="v-container">
-      <router-view
-          v-slot="{ Component }"
+    <div
+        class="v-container"
+    >
+      <div
+          v-if="!isMobile"
+          class="desktop-container"
       >
-        <transition
-            name="slide-fade"
+        <router-view
+            v-slot="{ Component }"
         >
+          <transition
+              name="slide-fade"
+          >
+            <component
+                :is="Component"
+                style="transition: .5s ease"
+            />
+          </transition>
+        </router-view>
+      </div>
+      <div
+          v-else
+          class="mobile-container v-scroll"
+          ref="content"
+      >
+        <template
+            v-for="route in routerComponents"
+            :key="route.name">
           <component
-              :is="Component"
-              style="transition: .5s ease"
+              :is="route.component"
+              :ref="route.name"
           />
-        </transition>
-      </router-view>
+        </template>
+      </div>
     </div>
   </main>
 </template>
 
 <script>
+
 export default {
-  name: "VContent"
+  name: "VContent",
+  props: {
+    isMobile: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    routerComponents() {
+      return this.$router['options']?.routes
+    },
+  }
 }
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 .v-container {
   position: relative;
+  scroll-behavior: smooth;
+  width: 100%;
+  height: 100%;
   overflow: hidden;
+  padding: 0;
+}
+
+.desktop-container, .mobile-container {
+  height: 100%;
+  width: 100%;
+}
+
+.desktop-container {
+  &::-webkit-scrollbar {
+    display: none;
+  }
 }
 
 .v-content {
-  background: #fff;
   width: 100%;
   height: 100%;
-  box-shadow: 0 2px 8px 0 black;
-  border-radius: 10px;
-  padding-top: 15px;
-  padding-bottom: 15px;
+  overflow: hidden;
 }
 
 .slide-fade-enter {
   transition: all .5s ease;
 }
+
 .slide-fade-enter-active {
   transform: translateX(-50%);
 }
@@ -49,9 +94,17 @@ export default {
 .slide-fade-enter-active, .slide-fade-leave-active {
   transition: all .5s ease;
   opacity: 0;
-
 }
+
 .slide-fade-leave-active {
   transform: translateX(50%);
+}
+
+.show-scroll {
+  background: #000;
+
+  ::-webkit-scrollbar {
+    display: block;
+  }
 }
 </style>
