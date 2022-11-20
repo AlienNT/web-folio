b1cee4874b8403ff48a50b09b97dff60ca052036
AlienNT <nick.tristen22@gmail.com>
2022-11-20 02:16:43 +0200

    test

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
       }
     }
   },
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/web-folio/'
-      : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //     ? '/web-folio/'
+  //     : '/'
 })
