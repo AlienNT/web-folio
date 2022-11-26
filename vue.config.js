b38eb83fd549019905c52ad25459c24666cffc75
AlienNT <nick.tristen22@gmail.com>
2022-11-26 21:31:26 +0200

    fix vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: `@import '@/assets/css/variables.scss';`
-      }
+    transpileDependencies: true,
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData: `@import '@/assets/css/variables.scss';`
+            }
+        }
+    },
+    lintOnSave: true,
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/web-folio/'
+        : '/',
+    devServer: {
+        proxy: process.env.NODE_ENV === 'production'
+            ? 'https://fake-json-server-nt.herokuapp.com'
+            : 'http://localhost:7000/api'
     }
-  },
-  lintOnSave: true,
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/web-folio/'
-      : '/',
-  devServer: {
-    proxy: 'https://fake-json-server-nt.herokuapp.com'
-  }
 })
